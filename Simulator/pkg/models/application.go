400f7fa479d159312ee958b619578b69aa19cd51
Remco Ploeg <49752333+rploeg@users.noreply.github.com>
2022-05-21 12:41:22 +0200

    Update application.go

--- Simulator/pkg/models/application.go
+++ Simulator/pkg/models/application.go
@@ -5,6 +5,6 @@ type (
 		ProvisioningURL    string `json:"provisioningUrl"`    // DPS provisioning URL.
 		IDScope            string `json:"idScope"`            // the id scope of the provisioning endpoint.
 		MasterKey          string `json:"masterKey"`          // the master SAS key of the provisioning endpoint.
-		BoltMachineModelID string `json:"boltMachineModelID"` // the bolt machine device model ID.
+		MachineModelID string `json:"MachineModelID"` // the bolt machine device model ID.
 	}
 )
